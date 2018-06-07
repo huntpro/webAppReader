fde7068bcf4ffef6fa57855583c7cc5e3a437b10
hunt <jiyingcai89@126.com>
2018-06-07 16:53:03 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const route = require('koa-route')
 const static = require('koa-static')
 const views = require('koa-views')
 const path = require('path')
-
-
 const app = new Koa()
 app.use(static(path.join(__dirname, '/static')))
 
 const index = ctx => {
     ctx.body = 'hello'
 }
+
+const main = ctx => ctx.body = 'main'
+
+app.use(route.get('/main', main))
 app.use(route.get('/index', index))
 
 
